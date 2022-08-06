625fa814454a472ca720d8fcb5cc62fb5b5429c1
Richard Engelke <somethingcivilised@gmail.com>
2022-08-06 18:27:28 +0930

    Increase Score + Some of Prestige

---
+++ TestComp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    props: {
+        upgradeBox: 'upgradeBox',
+    }
+}
+</script>
+
+<template>
+    <div :class="upgradeBox">
+        <button @click=attemptIncrement()>Prestige</button>
+    </div>
+</template>
+
